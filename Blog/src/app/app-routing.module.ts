3ed22b49274bf1b9e052aa9b5bbb089e0ca464f2
mirandaweathers <mirandaweathers@gmail.com>
2022-11-19 13:23:14 -0500

    add routes

--- Blog/src/app/app-routing.module.ts
+++ Blog/src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditaccountComponent } from './editaccount/editaccount.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
+import { ViewpostsComponent } from './viewposts/viewposts.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomepageComponent
+    component: ViewpostsComponent
   },
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'account',
+    component: EditaccountComponent
   }
 ];
 
