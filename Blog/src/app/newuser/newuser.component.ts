3847fab8203f7ca57aa9ee3abc7f29fea478a37c
mirandaweathers <mirandaweathers@gmail.com>
2022-11-19 16:14:22 -0500

    new user first logic

--- Blog/src/app/newuser/newuser.component.ts
+++ Blog/src/app/newuser/newuser.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./newuser.component.css']
 })
 export class NewuserComponent {
-
+  newUser = {
+    firstName: '',
+    lastName: '',
+    emailAddress: '',
+    password: ''
+  }
 }
