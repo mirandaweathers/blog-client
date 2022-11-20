93e8ab2797ca30d71d466f6f51120c0e70a1d425
mirandaweathers <mirandaweathers@gmail.com>
2022-11-20 16:12:35 -0500

    user model

---
+++ Blog/src/app/models/user.ts
@@ -0,0 +1,15 @@
+export class User {
+    userId:string = "";
+    firstName:string = "";
+    lastName:string = "";
+    emailAddress:string = "";
+    password:string = "";
+
+    constructor(userId:string, firstName:string, lastName:string, emailAddress:string, password:string) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.password = password;
+    }
+}
