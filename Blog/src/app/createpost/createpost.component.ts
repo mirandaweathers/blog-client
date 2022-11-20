63ca9fcea27973000014608cd1b254df43d9a76c
mirandaweathers <mirandaweathers@gmail.com>
2022-11-20 16:11:41 -0500

    add Create Post method to button click

--- Blog/src/app/createpost/createpost.component.ts
+++ Blog/src/app/createpost/createpost.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../models/post';
 
 @Component({
   selector: 'app-createpost',
   templateUrl: './createpost.component.html',
   styleUrls: ['./createpost.component.css']
 })
-export class CreatepostComponent {
-  newPost = {
-    title: '',
-    content: '',
-    headerImage: ''
+export class CreatepostComponent implements OnInit {
+  newPost:Post;
+
+  constructor() {
+    this.newPost = new Post('','','');
   }
+
+  CreatePost() {
+    console.log(this.newPost);
+  }
+
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+  }
+  
 }
