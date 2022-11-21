d406a54eb25c0cf99835a01f3b954da1010afd10
mirandaweathers <mirandaweathers@gmail.com>
2022-11-21 12:24:52 -0500

    client-side login functionality

--- Blog/src/app/login/login.component.ts
+++ Blog/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  userId = '';
+  password = '';
+  error = false;
 
+  constructor(private authsvc:AuthService, private router:Router) {
+    
+  }
+
+  Login() {
+    let result = this.authsvc.Login(this.userId, this.password);
+    if(result) {
+      this.router.navigate(['/']);
+    } else {
+      this.error = true;
+    }
+  }
 }
